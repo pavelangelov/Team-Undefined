f21b41bda4207e87159a15f12c58a5d8d20aa0a5
pavelangelov <p_a_v_el@abv.bg>
2016-11-21 19:09:18 +0200

    Initial commit.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+let port = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+
+    // ejs render automatically looks in the views folder
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Our app is running on http://localhost:${port}`);
+});
