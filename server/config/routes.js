622a9f756f52235906962af12579992a41f875fa
pavelangelov <p_a_v_el@abv.bg>
2016-11-26 19:58:52 +0200

    Small changes

--- server/config/routes.js
+++ server/config/routes.js
@@ -17,10 +17,17 @@ module.exports = (app) => {
             });
     });
 
-    app.get("/messages/:username", (req, res) => {
+    app.get("/users/:username/messages", (req, res) => {
         controler.users.getUserByUsername(req.params.username)
             .then(user => {
                 res.render("user-messages", { user, messages: user.messages });
+            });
+    });
+
+    app.get("/users/:username/profile", (req, res) => {
+        controler.users.getUserByUsername("pavel")
+            .then(user => {
+                res.render("user-profile", { user });
             });
     });
 
