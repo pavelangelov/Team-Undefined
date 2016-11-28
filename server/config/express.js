79607341ad64a0f7ee56138bef83eb3fb66d510e
pavelangelov <p_a_v_el@abv.bg>
2016-11-28 21:19:51 +0200

    Replaced get with post method in index page

--- server/config/express.js
+++ server/config/express.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 module.exports = (config, app) => {
     app.set("view engine", "pug");
     app.set("views", `${config.path.rootPath}/server/views`);
 
     // here load other routs
+    app.use(bodyParser.urlencoded({ extended: false }));
     app.use("/static", router);
     app.use("/static", express.static(`${config.path.rootPath}/public`));
 };
--- server/config/routes.js
+++ server/config/routes.js
@@ -5,39 +5,47 @@ const controler = require("../controllers");
 module.exports = (app) => {
     app.get("/", (req, res) => {
         res.render("index");
-    });
+    })
+        .post("/login", (req, res) => {
+            let username = req.body.email,
+                password = req.body.password;
 
-    app.get("/login", (req, res) => {
-        controler.users.getUserByUsername("pavel")
-            .then(user => {
-                controler.users.getPostsByUserId(user._id)
-                    .then(posts => {
-                        res.render("user-home", { user, posts });
-                    });
-            });
-    });
+            controler.users.getUserByUsername(username)
+                .then(user => {
+                    controler.users.getPostsByUserId(user._id)
+                        .then(posts => {
+                            res.render("user-home", { user, posts });
+                        });
+                });
+        })
+        .post("/register", (req, res) => {
+            let email = req.body.email,
+                password = req.body.password,
+                firstname = req.body.firstname,
+                lastname = req.body.lastname;
 
-    app.get("/users/:username/messages", (req, res) => {
-        controler.users.getUserByUsername(req.params.username)
-            .then(user => {
-                res.render("user-messages", { user, messages: user.messages });
-            });
-    });
+            // TODO: check all value and escape bad symbols
+            res.send({ email, password, firstname, lastname });
 
-    app.get("/users/:username/profile", (req, res) => {
-        controler.users.getUserByUsername("pavel")
-            .then(user => {
-                res.render("user-profile", { user });
-            });
-    });
-
-    app.get("/logout", (req, res) => {
-        res.redirect("/");
-    });
-
-    app.all("*", (req, res) => {
-        res.status(404);
-        res.send("Not Found");
-        res.end();
-    });
+        })
+        .get("/users/:username/messages", (req, res) => {
+            controler.users.getUserByUsername(req.params.username)
+                .then(user => {
+                    res.render("user-messages", { user, messages: user.messages });
+                });
+        })
+        .get("/users/:username/profile", (req, res) => {
+            controler.users.getUserByUsername("pavel")
+                .then(user => {
+                    res.render("user-profile", { user });
+                });
+        })
+        .get("/logout", (req, res) => {
+            res.redirect("/");
+        })
+        .all("*", (req, res) => {
+            res.status(404);
+            res.send("Not Found");
+            res.end();
+        });
 };
